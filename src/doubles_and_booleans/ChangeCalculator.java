addecd878b560dc871470f265f2037cce78272b4
598659 <598659@0338S-S-R0B6H0L.sdcs.local>
2018-09-18 14:25:59 -0700

    Change Calculator ip

--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -15,13 +15,15 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
 		// Convert their answer to an int using Integer.parseInt()
-
+		String n = JOptionPane.showInputDialog("How many nikels do you have");
+		int numN = Integer.parseInt(n);
 		// Ask the user how many dimes they have, and convert their answer
-
+		String d = JOptionPane.showInputDialog("How many dimes do you have");
+		int numD = Integer.parseInt(d);
 		// Ask the user how many quarters they have, and convert their answer
-
+		String q = JOptionPane.showInputDialog("How many quarters do you have");
+		int numQ = Integer.parseInt(q);
 		// Calculate how much money the user has and save it in a double variable 
 
 		// Tell the user how much money they have
