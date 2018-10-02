f051c3ec147d395a5e30eb0958132f124a250bb5
598659 <598659@0338S-S-R0B6H0L.sdcs.local>
2018-10-02 14:24:57 -0700

    Sleepy Head, String Methods 1, Pi Aloud ip

--- src/charAt_scanners_optionDialogs/PiAloud.java
+++ src/charAt_scanners_optionDialogs/PiAloud.java
@@ -9,9 +9,11 @@ import java.util.Scanner;
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
+public static void main(String[] args) {
+	
 
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-
+String pi = ""
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
@@ -22,7 +24,7 @@ public class PiAloud {
 	// *6. Get a character from the user using the getInputFromUser() method
 	// *7. Compare the users' char to the next digit of Pi
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
-
+}
 	static void speak(char characterToSpeak) {
 		try {
 			Runtime.getRuntime().exec("say " + characterToSpeak).waitFor();
--- src/doubles_and_booleans/SleepyHead.java
+++ src/doubles_and_booleans/SleepyHead.java
@@ -11,20 +11,27 @@ public class SleepyHead {
     public static void main(String[] args) {
         
         boolean isWeekday, isVacation;
-        
         /* You MUST use the above boolean variables in your code */
-        
+
         /*
          * Ask the user for these values using a confirm dialog like the one below
          * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
          */
-    
-
+        int answer = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+        int answer2 = JOptionPane.showConfirmDialog(null, "Are you on a vacation?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
 
         /*
          * Print â€œsleep inâ€? if it is a vacation or a weekend. If itâ€™s a weekday,
          * print â€œget up lazybones!â€? If it is a weekday, and we are on vacation,
          * print â€œsleep inâ€?.
          */
+        if(answer == 0) {isWeekday = true;}
+        else {isWeekday = false;}
+        if(answer2 == 0) {isVacation = true;}
+        else {isVacation = false;}
+        if (isWeekday) {System.out.println("â€œget up lazybones!â€?");}
+        if (isVacation && isWeekday == false) {System.out.println("â€œsleep inâ€?");}
+        if (isVacation && isWeekday) {System.out.println("â€œsleep inâ€?");}
+        else {System.out.println("");}
     }
 }
