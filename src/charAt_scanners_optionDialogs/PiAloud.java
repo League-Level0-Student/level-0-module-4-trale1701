1f719afeb1bc5527fa18778370924e206dd47464
598659 <598659@0338S-S-R0B6H0L.sdcs.local>
2018-10-09 14:26:32 -0700

    Pi Aloud

--- src/charAt_scanners_optionDialogs/PiAloud.java
+++ src/charAt_scanners_optionDialogs/PiAloud.java
@@ -6,6 +6,8 @@ package charAt_scanners_optionDialogs;
 import java.io.IOException;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
@@ -13,16 +15,22 @@ public static void main(String[] args) {
 	
 
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-String pi = ""
+String pi = "3.1415926535897932384626";
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
-
+System.out.println(pi.charAt(0));
+System.out.println(pi.charAt(1));
+System.out.println(pi.charAt(2));
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
-
+for (int i = 0; i < pi.length(); i++) {
+	System.out.print(pi.charAt(i));
+}
 	// 5. Use the speak() method to speak all the digits of Pi.
 
 	// [ADVANCED]
 	// *6. Get a character from the user using the getInputFromUser() method
+String answer = JOptionPane.showInputDialog("what is the forth digit of PI?");
 	// *7. Compare the users' char to the next digit of Pi
+
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
 }
 	static void speak(char characterToSpeak) {
